076d086ebed63b171fe4f6b5ec3eeda52c47e2f5
ParkSangKyu <108622613+sangkyu39@users.noreply.github.com>
2023-04-14 19:13:44 +0900

    2번 수정

--- week.7/2.c
+++ week.7/2.c
@@ -2,141 +2,141 @@
 #include <stdlib.h>
 
 typedef struct Node {
-	int elem;
-	struct Node* next;
+   int elem;
+   struct Node* next;
 } Node;
 
 typedef struct List {
-	Node* header;
+   Node* header;
 } List;
 
 void init(List* list) {
-	list->header = (Node*)calloc(1, sizeof(Node));
+   list->header = (Node*)calloc(1, sizeof(Node));
 }
 
 void addTerm(List* list, int elem) {
-	Node* p = list->header;
-	Node* node = (Node*)calloc(1, sizeof(Node));
+   Node* p = list->header;
+   Node* node = (Node*)calloc(1, sizeof(Node));
 
-	while (p->next) {
-		p = p->next;
-	}
+   while (p->next) {
+      p = p->next;
+   }
 
-	if (p->elem == NULL) {
-		p->elem = elem;
-	}
-	else {
-		node->elem = elem;
-		p->next = node;
-	}
+   if (p->elem == NULL) {
+      p->elem = elem;
+   }
+   else {
+      node->elem = elem;
+      p->next = node;
+   }
 }
 
 void print(List* list) {
-	Node* p = list->header;
+   Node* p = list->header;
 
-	while (p) {
-		printf(" %d", p->elem);
-		p = p->next;
-	}
+   while (p) {
+      printf(" %d", p->elem);
+      p = p->next;
+   }
 
-	printf("\n");
+   printf("\n");
 }
 
 void setUnion(List* list1, List * list2) {
-	List result;
+   List result;
 
-	init(&result);
+   init(&result);
 
-	Node* node1 = list2->header;
-	Node* node2 = list1->header;
+   Node* node1 = list2->header;
+   Node* node2 = list1->header;
 
-	while (node1 || node2) {
-		if (node1 == NULL) {
-			addTerm(&result, node2->elem);
-			node2 = node2->next;
-		}
-		else if (node2 == NULL) {
-			addTerm(&result, node1->elem);
-			node1 = node1->next;
-		}
-		else if (node1->elem < node2->elem) {
-			addTerm(&result, node1->elem);
-			node1 = node1->next;
-		}
-		else if (node2->elem < node1->elem) {
-			addTerm(&result, node2->elem);
-			node2 = node2->next;
-		}
-		else if (node1->elem == node2->elem) {
-			addTerm(&result, node1->elem);
-			node1 = node1->next;
-			node2 = node2->next;
-		}
-		
-	}
+   while (node1 || node2) {
+      if (node1 == NULL) {
+         addTerm(&result, node2->elem);
+         node2 = node2->next;
+      }
+      else if (node2 == NULL) {
+         addTerm(&result, node1->elem);
+         node1 = node1->next;
+      }
+      else if (node1->elem < node2->elem) {
+         addTerm(&result, node1->elem);
+         node1 = node1->next;
+      }
+      else if (node2->elem < node1->elem) {
+         addTerm(&result, node2->elem);
+         node2 = node2->next;
+      }
+      else if (node1->elem == node2->elem) {
+         addTerm(&result, node1->elem);
+         node1 = node1->next;
+         node2 = node2->next;
+      }
+      
+   }
 
-	print(&result);
+   print(&result);
 }
 
 int setSubtract (List* list1, List* list2) {
-	List result;
+   List result;
 
-	init(&result);
+   init(&result);
 
-	Node* node1 = list2->header;
-	Node* node2 = list1->header;
+   Node* node1 = list2->header;
+   Node* node2 = list1->header;
 
-	while (node1 && node2) {
-		if (node1 == NULL) {
-			addTerm(&result, node2->elem);
-			node2 = node2->next;
-		}
-		else if (node2 == NULL) {
-			addTerm(&result, node1->elem);
-			node1 = node1->next;
-		}
-		else if (node1->elem < node2->elem) {
-			node1 = node1->next;
-		}
-		else if (node2->elem < node1->elem) {
-			node2 = node2->next;
-		}
-		else if (node1->elem == node2->elem) {
-			addTerm(&result, node1->elem);
-			node1 = node1->next;
-			node2 = node2->next;
-		}
+   while (node1 && node2) {
+      if (node1 == NULL) {
+         addTerm(&result, node2->elem);
+         node2 = node2->next;
+      }
+      else if (node2 == NULL) {
+         addTerm(&result, node1->elem);
+         node1 = node1->next;
+      }
+      else if (node1->elem < node2->elem) {
+         node1 = node1->next;
+      }
+      else if (node2->elem < node1->elem) {
+         node2 = node2->next;
+      }
+      else if (node1->elem == node2->elem) {
+         addTerm(&result, node1->elem);
+         node1 = node1->next;
+         node2 = node2->next;
+      }
 
-	}
+   }
 
-	print(&result);
+   print(&result);
 }
 
 int main() {
-	List list1, list2;
+   List list1, list2;
 
-	init(&list1);
-	init(&list2);
+   init(&list1);
+   init(&list2);
 
-	int cnt, elem;
+   int cnt, elem;
 
-	scanf("%d", &cnt);
+   scanf("%d", &cnt);
 
-	for (int i = 0; i < cnt; i++) {
-		scanf("%d", &elem);
-		addTerm(&list1, elem);
-	}
+   for (int i = 0; i < cnt; i++) {
+      scanf("%d", &elem);
+      addTerm(&list1, elem);
+   }
 
-	scanf("%d", &cnt);
+   scanf("%d", &cnt);
 
-	for (int i = 0; i < cnt; i++) {
-		scanf("%d", &elem);
-		addTerm(&list2, elem);
-	}
+   for (int i = 0; i < cnt; i++) {
+      scanf("%d", &elem);
+      addTerm(&list2, elem);
+   }
 
-	setUnion(&list1, &list2);
+   setUnion(&list1, &list2);
 
-	setSubtract(&list1, &list2);
+   setSubtract(&list1, &list2);
 
-	return 0;
+   return 0;
 }
